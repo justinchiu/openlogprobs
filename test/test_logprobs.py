d7815060f8652f5847a0598c71d014f02baac8f7
Justin Chiu <justinchiu@g.harvard.edu>
2023-11-27 17:55:03 -0500

    start test

---
+++ test/test_logprobs.py
@@ -0,0 +1,11 @@
+from open_logprobs.extract import extract_logprobs, bisection_search, topk_search
+
+def test_bisection():
+    prefix = ""
+    assert False
+
+def test_topk():
+    assert False
+
+def test_extract():
+    assert False
