e21341431287c0e09bd7f48a1989e4237fb26515
Justin Chiu <chiu.justin.t@gmail.com>
2023-11-27 20:22:01 -0500

    start tests

--- test/test_logprobs.py
+++ test/test_logprobs.py
@@ -1,7 +1,16 @@
-from open_logprobs.extract import extract_logprobs, bisection_search, topk_search
+from open_logprobs.extract import (
+    extract_logprobs,
+    bisection_search,
+    topk_search,
+    topk as topk_,
+)
+
+prefix = "Should i take this class or not? The professor of this class is not good at all. He doesn't teach well and he is always late for class."
+model = "gpt-3.5-turbo-instruct"
+topk_words = topk_(model, prefix)
 
 def test_bisection():
-    prefix = ""
+
     assert False
 
 def test_topk():
