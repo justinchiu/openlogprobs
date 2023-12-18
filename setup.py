b85fa5510bcefe604d7304d1ad07534493c2e8e8
Jack Morris <jxmorris12@gmail.com>
2023-12-18 12:07:57 -0500

    subtle rename

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
-import setuptools
+from setuptools import find_packages, setup
 
-if __name__ == "__main__":
-    setuptools.setup()
+setup(
+    name="openlogprobs",
+    version="0.0.1",
+    description="extract log-probabilities from APIs",
+    author="Justin Chiu & Jack Morris",
+    author_email="jtc257@cornell.edu",
+    packages=find_packages(),
+    install_requires=open("requirements.txt").readlines()
+    # install_requires=[],
+)
