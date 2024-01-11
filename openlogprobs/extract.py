fixed numerical instability

openlogprobs/extract.py
@@ -3,29 +3,48 @@ import tiktoken
 import numpy as np
 from scipy.special import logsumexp
 import math
+from functools import partial, reduce
+from operator import or_ as union
 from typing import Literal
 
-from concurrent.futures import ThreadPoolExecutor, as_completed
+from concurrent.futures import ThreadPoolExecutor
 
 from openlogprobs.models import Model
-from openlogprobs.utils import LockedOutput, batched
+from openlogprobs.utils import batched
 
 
-def exact_solve(model: Model, prefix: str, idx: int, bias=20):
-    """Exact solve based on https://mattf1n.github.io/openlogprobs.html"""
-    return parallel_exact_solve(model, prefix, [idx], bias=bias)
-
-
-def parallel_exact_solve(model: Model, prefix: str, idx: list[int], bias: float = 20.0):
+def exact_solve(
+    model: Model,
+    prefix: str,
+    idx: list[int],
+    bias: float = 5.0,
+    top_logprob: float | None = None,
+) -> tuple[dict[int, float], set[int], int]:
     """Parallel exact solve based on https://mattf1n.github.io/openlogprobs.html"""
     logit_bias = {i: bias for i in idx}
-    lmbda = 1 / np.exp(bias)
-    loglmbda = np.log(1) - bias
     topk_words = model.topk(prefix, logit_bias)
-    biased_logprobs = np.array([topk_words[i] for i in idx])
-    biased_probs_sum = np.exp(biased_logprobs).sum()
-    logprobs = loglmbda + biased_logprobs - np.log(1 + (lmbda - 1) * biased_probs_sum)
-    return logprobs, 1
+    if all(i in topk_words for i in idx):
+        biased_logprobs = np.array([topk_words[i] for i in idx])
+        log_biased_prob = logsumexp(biased_logprobs)
+        logprobs = biased_logprobs - np.logaddexp(
+            bias + np.log1p(-np.exp(log_biased_prob)), log_biased_prob
+        )
+        return dict(zip(idx, logprobs)), set(), 1
+    else:
+        if top_logprob is None:
+            missing_tokens = set(idx) - set(topk_words)
+            raise TypeError(
+                f"Tokens {missing_tokens} not in top-k with bias {bias}."
+                "Either increase bias or provide top unbiased logprob (top_logprob)"
+            )
+        success_idxs = list(i for i in idx if i in topk_words)
+        fail_idxs = set(idx) - set(topk_words)
+        biased_top_logprob = max(
+            logprob for i, logprob in topk_words.items() if i not in idx
+        )
+        biased_logprobs = np.array([topk_words[i] for i in success_idxs])
+        logprobs = biased_logprobs - biased_top_logprob + top_logprob - bias
+        return dict(zip(success_idxs, logprobs)), fail_idxs, 1
 
 
 def bisection_search(
@@ -93,40 +112,51 @@ def extract_logprobs(
     k: int = 5,
     eps: float = 1e-6,
     multithread: bool = False,
-    bias: float = 20.0,
+    bias: float = 5.0,
     parallel: bool = False,
 ):
     vocab_size = model.vocab_size
-    output = LockedOutput(vocab_size, total_calls=0)
 
-    search = (
-        topk_search
-        if method == "topk"
-        else bisection_search
-        if method == "bisection"
-        else parallel_exact_solve
-        if method == "exact" and parallel
-        else exact_solve
-    )
-
-    def worker(x, output):
-        search_kwargs = dict(bias=bias) if method == "exact" else dict(k=k)
-        logprob, num_calls = search(model, prefix=prefix, idx=x, **search_kwargs)
-        output.add(num_calls, x, logprob)
-
-    if method == "exact" and parallel:
-        vocab = list(map(list, batched(range(vocab_size), k)))
+    if method == "exact":
+        logprob_dict = model.topk(prefix)
+        top_logprob = max(logprob_dict.values())
+        bias += top_logprob - min(logprob_dict.values())
+        remaining = set(range(vocab_size)) - set(logprob_dict)
+        total_calls = 0
+        if multithread:
+            executor = ThreadPoolExecutor(max_workers=8)
+            map_func = executor.map
+        else:
+            map_func = map
+        while remaining:
+            search_results = map_func(
+                partial(
+                    exact_solve,
+                    model,
+                    prefix,
+                    bias=bias,
+                    top_logprob=top_logprob,
+                ),
+                batched(remaining, k),
+            )
+            logprob_dicts, skipped, calls = zip(*search_results)
+            logprob_dict |= reduce(union, logprob_dicts)
+            remaining = set.union(*skipped)
+            total_calls += sum(calls)
+            bias += 5
+        if multithread:
+            executor.shutdown()
+        logprobs = np.array([logprob_dict[i] for i in range(vocab_size)])
+        return logprobs, total_calls
     else:
+        search_func = topk_search if method == "topk" else bisection_search
+        search = partial(search_func, model, prefix, k=k)
         vocab = list(range(vocab_size))
-
-    if multithread:
-        with tqdm.tqdm(total=len(vocab)) as pbar:
-            with ThreadPoolExecutor(max_workers=8) as pool:
-                futures = [pool.submit(worker, x, output) for x in vocab]
-                for future in as_completed(futures):
-                    pbar.update(1)
-    else:
-        for x in tqdm.tqdm(vocab):
-            worker(x, output)
-
-    return output.logits - logsumexp(output.logits), output.total_calls
+        if multithread:
+            with ThreadPoolExecutor(max_workers=8) as executor:
+                search_results = executor.map(search, tqdm.tqdm(vocab))
+        else:
+            search_results = map(search, tqdm.tqdm(vocab))
+        logit_list, calls = zip(*search_results)
+        logits = np.array(logit_list)
+        return logits - logsumexp(logits), sum(calls)

test/test_logprobs.py
@@ -21,21 +21,24 @@ prefix = "Should i take this class or not? The professor of this class is not go
 def load_fake_logits(vocab_size: int) -> np.ndarray:
     np.random.seed(42)
     logits = np.random.randn(vocab_size)
-    logits[1] += 1
-    logits[12] += 2
-    logits[13] += 3
-    logits[24] += 3
-    logits[35] += 3
+    logits[1] += 10
+    logits[12] += 20
+    logits[13] += 30
+    logits[24] += 30
+    logits[35] += 30
     return logits
 
 
 class FakeModel(Model):
     """Represents a fake API with a temperature of 1. Used for testing."""
 
-    def __init__(self, vocab_size: int = 100):
+    def __init__(self, vocab_size: int = 100, get_logits=None):
         self.tokenizer = transformers.AutoTokenizer.from_pretrained("gpt2")
         self.fake_vocab_size = vocab_size
-        self.logits = load_fake_logits(self.vocab_size)[:vocab_size]
+        if get_logits is None:
+            self.logits = load_fake_logits(self.vocab_size)[:vocab_size]
+        else:
+            self.logits = get_logits(vocab_size)
 
     @property
     def vocab_size(self):
@@ -66,6 +69,12 @@ class FakeModel(Model):
 def model():
     # return OpenAIModel("gpt-3.5-turbo-instruct")
     return FakeModel()
+
+
+@pytest.fixture
+def uniform_model():
+    # return OpenAIModel("gpt-3.5-turbo-instruct")
+    return FakeModel(get_logits=np.ones)
 
 
 @pytest.fixture
@@ -123,7 +132,7 @@ def test_extract_bisection(model):
         model, prefix="test", method="bisection", multithread=False, k=1
     )
     np.testing.assert_allclose(true_logprobs, extracted_logprobs)
-    assert num_calls == 3268
+    assert num_calls == 3270
 
 
 def test_extract_exact(model):
@@ -132,6 +141,7 @@ def test_extract_exact(model):
         model, prefix="test", method="exact", multithread=False
     )
     np.testing.assert_allclose(true_logprobs, extracted_logprobs)
+    assert num_calls < len(true_logprobs)
 
 
 def test_extract_exact_parallel(model):
@@ -142,11 +152,9 @@ def test_extract_exact_parallel(model):
         method="exact",
         multithread=False,
         parallel=True,
-        bias=20,
     )
-    print(true_logprobs)
-    print(extracted_logprobs)
     np.testing.assert_allclose(true_logprobs, extracted_logprobs)
+    assert num_calls < len(true_logprobs)
 
 
 def test_extract_topk_multithread(model):
@@ -164,11 +172,25 @@ def test_extract_exact_multithread(model):
         model, prefix="test", method="exact", multithread=True
     )
     np.testing.assert_allclose(true_logprobs, extracted_logprobs)
+    assert num_calls < len(true_logprobs)
 
 
 def test_extract_exact_parallel_multithread(model):
     true_logprobs = log_softmax(model.logits)
     extracted_logprobs, num_calls = extract_logprobs(
-        model, prefix="test", method="exact", multithread=True, parallel=True, bias=10
+        model, prefix="test", method="exact", multithread=True, parallel=True
     )
     np.testing.assert_allclose(true_logprobs, extracted_logprobs)
+    assert num_calls < len(true_logprobs)
+
+
+def test_extract_exact_parallel_multithread_uniform(uniform_model):
+    true_logprobs = log_softmax(uniform_model.logits)
+    extracted_logprobs, num_calls = extract_logprobs(
+        uniform_model,
+        prefix="test",
+        method="exact",
+        parallel=True,
+    )
+    np.testing.assert_allclose(true_logprobs, extracted_logprobs)
+    assert num_calls < len(true_logprobs)